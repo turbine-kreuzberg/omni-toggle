354823b852c3c58853f4d90357bba2050ec43039
Thomas Heuer <thomas.heuer@votum.de>
2016-04-18 18:42:32 +0200

    a basic test suite: test-module-api.js

--- test/test-module-api.js
+++ test/test-module-api.js
@@ -1,42 +1,28 @@
-define( ['toggle'], function( Toggle ) {
+describe( 'Basic setup', function() {
 
-    describe( 'module API', function() {
+    it( 'has Require.js loaded', function() {
+        define.should.be.Function();
+        define.should.have.property( 'amd' );
+    } );
 
-        it( 'has Require.js loaded', function() {
-            if( typeof define !== "function" || !define.amd ) {
-                throw new Error( 'Require.js was not loaded' );
-            }
+    describe( 'Check Toggle module', function() {
+        var Toggle;
+
+        beforeEach( function( done ) {
+            require( ['toggle'], function( _Toggle ) {
+                Toggle = _Toggle;
+                done();
+            } );
         } );
 
-        it( 'has module loaded', function() {
-            if( !Toggle ) {
-                throw new Error( 'The module Toggle could not be loaded.' );
-            }
+        it( 'can load module', function() {
+            Toggle.should.exist;
         } );
 
         it( 'has public method init()', function() {
-            if( typeof Toggle.init !== 'function' ) {
-                throw new Error( 'The module Toggle has no (public) init() method.' );
-            }
+            Toggle.init.should.be.Function();
         } );
 
     } );
 
 } );
-
-//describe( 'Testing module API', function() {
-//
-//    var Toggle;
-//
-//    beforeEach( function( done ) {
-//        require( ['src/toggle'], function( _Toggle ) {
-//            Toggle = _Toggle;
-//            done();
-//        } );
-//    } );
-//
-//    describe( 'module loaded', function() {
-//
-//    } );
-//
-//} );
